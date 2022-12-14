84aaa556476aee6158c34daae8bd80efc84baa15
Boris Lyubchenko <Blyubchenko@yandex.ru>
2022-12-14 23:45:20 +0700

    fix: замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,37 @@
 //Объявляю основные переменные
 let editProfile = document.querySelector('.profile__edit-button');
-let profilName = document.querySelector('.profile__title');
-let profilJob = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector('.popup');
-let nameInput = formElement.querySelector('.popup__input');
-let jobInput = formElement.querySelector('.popup__input:last-child');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__container');
+let closeBotton = document.querySelector('.popup__button-close');
+let nameInput = formElement.querySelector('#name');
+let jobInput = formElement.querySelector('#job');
+const popup = document.querySelector('.popup');
 
 //Реализую функцию открытия модального окна
 function popupOpen() {
-  document.querySelector('.popup').classList.add('popup_opened');
-  nameInput.value = profilName.textContent;
-  jobInput.value = profilJob.textContent;
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
-//Добавил слушателя для открытия модального окна
-editProfile.addEventListener('click', popupOpen);
+
 
 //Реализую функцию закрытия модального окна
-let closeBotton = document.querySelector('.popup__botton-close');
-
 function popupClose() {
-  document.querySelector('.popup').classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
-//Добавил слушателя для закрытия модального окна
-closeBotton.addEventListener('click', popupClose);
 
 //Реализовал функцию редактирования профиля
 function handleFormSubmit (evt) {
     evt.preventDefault();
-
-    profilName.textContent = nameInput.value;
-
-    profilJob.textContent = jobInput.value;
-
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     popupClose();
 }
+
+//Добавил слушателя для открытия модального окна
+editProfile.addEventListener('click', popupOpen);
+//Добавил слушателя для закрытия модального окна
+closeBotton.addEventListener('click', popupClose);
 //Добавил слушателя для раедактирования профиля
 formElement.addEventListener('submit', handleFormSubmit);
