1c8118493a5c2c19b1f7a940a885eaf1a84e6e72
Boris Lyubchenko <BLyubchenko@yandex.ru>
2023-01-13 04:35:41 +0800

    fix: синтаксическая ошибка

--- scripts/index.js
+++ scripts/index.js
@@ -54,27 +54,27 @@ const initialCards = [
     name: 'Домбай',
     link: 'images/card/card_1.jpg',
     alt: 'Гора Домбай'
-  }
+  },
   {
     name: 'Телецкое',
     link: 'images/card/card_2.jpg',
     alt: 'Телецкое озеро'
-  }
+  },
   {
     name: 'Гамсутль',
     link: 'images/card/card_3.jpg',
     alt: 'Поселение Гамсутль в Дагестане'
-  }
+  },
   {
     name: 'Ольхон',
     link: 'images/card/card_4.jpg',
     alt: 'Гора Шаманка на острове Ольхон'
-  }
+  },
   {
     name: 'Камчатка',
     link: 'images/card/card_5.jpg',
     alt: 'Гейзеры на Камчатке'
-  }
+  },
   {
     name: 'Саяны',
     link: 'images/card/card_6.jpg',
